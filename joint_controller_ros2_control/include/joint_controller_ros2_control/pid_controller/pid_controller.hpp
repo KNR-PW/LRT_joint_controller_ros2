de3cd69d6c685bb09781d67f9c8a62012f13af9f
Bartek <bartlomiejk@vp.pl>
2025-07-21 20:52:51 +0200

    Added GPL3 license in headers

--- joint_controller_ros2_control/include/joint_controller_ros2_control/pid_controller/pid_controller.hpp
+++ joint_controller_ros2_control/include/joint_controller_ros2_control/pid_controller/pid_controller.hpp
@@ -1,3 +1,24 @@
+// Copyright (c) 2025, Koło Naukowe Robotyków
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+/*
+ * Authors: Bartłomiej Krajewski (https://github.com/BartlomiejK2)
+            Stanisław Sufin      (https://github.com/Kafel1997)
+ */
+
+
 #ifndef _PID_CONTROLLER_HPP_
 #define _PID_CONTROLLER_HPP_
 
